707c9071101b7f5d0526a0fbbf6394b26068586a
Ben Hawley <ben.hawley@digitaldesignlabs.com>
2018-12-21 10:12:11 +0000

    Login Screen - Lab 5

---
+++ Login Screen/LayoutApp4/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LayoutApp4
+//
+//  Created by Ben Hawley on 09/12/2018.
+//  Copyright © 2018 Ben Hawley. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
